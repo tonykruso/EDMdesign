f3766839d2b4cd92dea88f325aa8ae751a4d5d13
Tony Kruso <tony.kruso@gmail.com>
2019-03-30 12:27:12 -0400

    added JS files

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -1,10 +1,95 @@
- // Initialize Firebase
- var config = {
-    apiKey: “AIzaSyA0W5CYMQlloBwy2vbfoXHQgZYUXNgLsqU”,
-    authDomain: “inclassproject-3da67.firebaseapp.com”,
-    databaseURL: “https://inclassproject-3da67.firebaseio.com”,
-    projectId: “inclassproject-3da67",
-    storageBucket: “inclassproject-3da67.appspot.com”,
-    messagingSenderId: “358462140316"
+var config = {
+    apiKey: "AIzaSyA0W5CYMQlloBwy2vbfoXHQgZYUXNgLsqU",
+    authDomain: "inclassproject-3da67.firebaseapp.com",
+    databaseURL: "https://inclassproject-3da67.firebaseio.com",
+    projectId: "inclassproject-3da67",
+    storageBucket: "inclassproject-3da67.appspot.com",
+    messagingSenderId: "358462140316"
   };
   firebase.initializeApp(config);
+
+var database = firebase.database()
+var index = 1
+var name = ""
+var role = ""
+var startDate = ""
+var monthlyRate = ""
+database.ref().on("child_added", function(snapshot) {
+var data = snapshot.val()
+console.log(data)
+    
+/* name = snapshot.val().name
+role = snapshot.val().role
+startDate = snapshot.val().startDate
+monthlyRate = snapshot.val().monthlyRate */
+
+createEmployees(data)
+})
+function createEmployees(data) {
+    var employee = data 
+    console.log(employee.length)
+    name = employee.name
+    role = employee.role
+    startDate = employee.startDate
+    monthlyRate = employee.monthlyRate
+
+    var row = $("<tr>")
+    var indextag = $("<th>").text("#" + index)
+    var nametag = $("<th>").text(name)
+    var roletag = $("<th>").text(role)
+    var starttag = $("<th>").text(startDate)
+    var ratetag = $("<th>").text(monthlyRate)
+    var monthtag = $("<th>").text(5)
+    var build = 5 * parseInt(monthlyRate)
+    var billedtag = $("<th>").text(build)
+    index++
+
+    row.append(indextag)
+    row.append(nametag)
+    row.append(roletag)
+    row.append(starttag)
+    row.append(ratetag)
+    row.append(monthtag)
+    row.append(billedtag)
+
+    $("#employee").append(row)
+}
+
+$("#submit").on("click", function(event) {
+   event.preventDefault()
+    name = $("#name").val().trim()
+    role = $("#role").val().trim()
+    startDate = $("#startDate").val().trim()
+    monthlyRate = $("#monthlyRate").val().trim()
+    database.ref().push({
+        name: name,
+        role: role,
+        startDate: startDate,
+        monthlyRate: monthlyRate,
+        timestamp: firebase.database.ServerValue.TIMESTAMP
+    })
+})
+ // Initialize Firebase
+ /*var config = {
+    apiKey: "AIzaSyA0W5CYMQlloBwy2vbfoXHQgZYUXNgLsqU",
+    authDomain: "inclassproject-3da67.firebaseapp.com",
+    databaseURL: "https://inclassproject-3da67.firebaseio.com",
+    projectId: "inclassproject-3da67",
+    storageBucket: "inclassproject-3da67.appspot.com",
+    messagingSenderId: "358462140316"
+  };
+  firebase.initializeApp(config);
+
+ //  dataAdded: firebase.database.ServerValue.TIMESTAMP
+
+  //child added and child changes 
+  dataRef.ref().on("child_added",function(childSnapshot){
+      console.log(childSnapshot.val().name);
+  },function(errorObject){
+      console.log("Errors handled: " + errorObject.code)
+  });
+
+  data.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(ChildSnapShot))
+  //change HTML to reflect
+  $("#name-display").text(snapshot.val().name);
+  email,age,comment same thing*/
