4d8fe91a97931e01541df438fbfa79cde6f48ec0
Tony Kruso <tony.kruso@gmail.com>
2019-03-30 11:00:54 -0400

    built bootstrap

---
+++ assets/js/apps.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: “AIzaSyA0W5CYMQlloBwy2vbfoXHQgZYUXNgLsqU”,
+    authDomain: “inclassproject-3da67.firebaseapp.com”,
+    databaseURL: “https://inclassproject-3da67.firebaseio.com”,
+    projectId: “inclassproject-3da67",
+    storageBucket: “inclassproject-3da67.appspot.com”,
+    messagingSenderId: “358462140316"
+  };
+  firebase.initializeApp(config);
